af289a50ac36cb5131bfae8cdd01266d1740425b
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-11-10 17:03:05 -0300

    up

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector("#menu")
+const close = document.querySelector("#close-menu")
+console.log(menu)
+
+
+const x = 10; //number
+console.log(typeof x);
+
+const lista = [1, 2, 3]; //object array
+console.log(typeof lista);
+
+const objeto = {name: "Kelve", age: 27}; //object
+console.log(typeof objeto);
+
+const y = true //boolean
+console.log(typeof y);
+
+//Estrtura de Condição
+if( 2 > 4) {
+    console.log("2 é maior que 1")
+}else{
+    console.log("É menor")
+}
+
+//Estrutura de Repetição
